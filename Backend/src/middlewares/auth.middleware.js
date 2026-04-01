19a0ff9c2cdf7a066ddc547a4e44cb457d050f6b
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2026-04-01 19:21:45 +0530

    improved file structure

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../../models/user.model.js";
+import User from "../models/user.model.js";
 
 // middleware to protect routes that require authentication
 export const authMiddleware = async (req, res, next) => {
