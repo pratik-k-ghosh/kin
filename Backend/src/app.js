a40fb71c78d4cfe6ab3c786d84adeb65aaeaf440
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2026-04-01 19:08:30 +0530

    updated app.js for better documentation

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,22 +1,28 @@
+// import all modules
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+// import all routes
 import { connectDb } from "./config/db.js";
 import postRoute from "./modules/post/post.route.js";
 import authRoute from "./modules/auth/auth.route.js";
 
+// create app instance
 const app = express();
 
+// configure the app to use middlewares
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
+// connect to the database
 connectDb();
 
+// use routes
 app.use("/api/post", postRoute);
 app.use("/api/auth", authRoute);
 
